c61e737d90383b246c5227d7753e074ebca6748f
Graham Preston <graham.preston@gmail.com>
2025-11-26 14:05:32 +0100

    test_eval.ml

---
+++ test/test_eval.ml
@@ -0,0 +1,59 @@
+open OUnit2
+open Typeur_evaluateur
+
+let eval_test_failure (res : Common.pterm) =
+  assert_bool (Common.print_term res) false
+
+let ex_id : Common.pterm = Abs ("x", Var "x")
+let ex_id_id_x : Common.pterm = App (ex_id, App (ex_id, Var "x"))
+let ex_let : Common.pterm = Let ("x", Add (N 1, N 2), Add (Var "x", N 10))
+
+let ex_fix : Common.pterm =
+  App
+    ( Fix
+        ( "phi",
+          Abs ("n", IfZero (Var "n", N 123, App (Var "phi", Sub (Var "n", N 1))))
+        ),
+      N 3 )
+
+let ex_ref : Common.pterm = Let ("r", Ref EmptyList, Deref (Var "r"))
+
+let ex_assign : Common.pterm =
+  Let
+    ( "l",
+      Ref EmptyList,
+      Let
+        ( "_",
+          Assign (Var "l", Cons (Abs ("x", Var "x"), EmptyList)),
+          Add (Head (Deref (Var "l")), N 2) ) )
+
+let assert_eval (term : Common.pterm) (expected : Common.pterm) =
+  let result = Evaluateur.eval term in
+  if result = expected then
+    ()
+  else
+    eval_test_failure result
+
+let eval_expectations : (string * Common.pterm * Common.pterm) list = [
+  ("test_id", ex_id, Abs ("x", Var "x"));
+  ("test_id_id_x", ex_id_id_x, Var "x");
+  ("test_let", ex_let, N 13);
+  ("test_fix", ex_fix, N 123);
+  ("test_ref", ex_ref, EmptyList);
+  ("test_assign", ex_assign, Add (Abs ("x", Var "x"), N 2));
+]
+
+let expectation_to_test (tup : string * Common.pterm * Common.pterm) =
+  let (name, term, expected) = tup in
+  name >:: fun _ -> assert_eval term expected
+
+let evaluateur_tests = List.map expectation_to_test eval_expectations
+
+(* >:: names a test. Then we put them in named groups, for organization with
+   >:::. *)
+
+let suite = "EvaluateurTests" >::: evaluateur_tests
+
+(* Run the test suite. *)
+let () =
+  run_test_tt_main suite
